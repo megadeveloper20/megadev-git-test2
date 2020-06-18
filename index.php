337f22443f550536919a275fc21a4129478a6a5c
Andre Jan Febry <janfebry.72@gmail.com>
2020-06-18 20:12:57 +0700

    Menambah list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, cumque amet incidunt adipisci asperiores veritatis quidem ullam maxime commodi illo aut deleniti laborum tempora eum dolores excepturi? Deleniti, ex architecto!</p>
+    <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+    </ul>
 </body>
 </html>
