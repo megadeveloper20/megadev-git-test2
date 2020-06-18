f26b371fa3a58a4794bb7219d54afe2f59fe78bc
Andre Jan Febry <janfebry.72@gmail.com>
2020-06-18 20:07:50 +0700

    Menambah Paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, cumque amet incidunt adipisci asperiores veritatis quidem ullam maxime commodi illo aut deleniti laborum tempora eum dolores excepturi? Deleniti, ex architecto!</p>
 </body>
 </html>
